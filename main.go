4beffe93459b848b49dc5fdbf9dcd543be3fb356
amolpol_98 <amol.pol@toppr.com>
2016-03-06 18:51:19 +0530

    channel related messages in go

--- main.go
+++ main.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gorilla/websocket"
+	"github.com/mitchellh/mapstructure"
+	"time"
 )
+
+type Message struct {
+	Name string			`json:"name"`
+	Data interface{}	`json:"data"`
+}
+
+type Channel struct {
+	Id string		`json:"id"`
+	Name string		`json:"name"`
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize: 	1024,
@@ -24,16 +36,59 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return 
 	}
 	for {
-		msgType, msg, err := socket.ReadMessage()
+		/*msgType, msg, err := socket.ReadMessage()
 		if err != nil {
 			fmt.Println(err)
 			return
+		}*/
+		var inMessage Message
+		var outMessage Message
+		if err := socket.ReadJSON(&inMessage); err != nil {
+			fmt.Println(err)
+			break
 		}
-		fmt.Println(string(msg))
+		fmt.Printf("%#v\n", inMessage)
+		switch inMessage.Name {
+		case "channel add":
+			err := addChannel(inMessage.Data)
+			if err != nil {
+				outMessage = Message{"error", err}
+				if err := socket.WriteJSON(outMessage); err != nil {
+					fmt.Println(err)
+					break
+				}
+			}
+		case "channel subscribe":
+			go subscribeChannel(socket)
+		/*fmt.Println(string(msg))
 		fmt.Println(string(msgType))
 		if err = socket.WriteMessage(msgType, msg); err != nil {
 			fmt.Println(err)
 			return
+		}*/
 		}
 	}
 }	
+
+func addChannel(data interface{}) (error) {
+	var channel Channel
+
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	channel.Id = "1"
+	fmt.Println("added channel")
+	return nil
+}
+
+func subscribeChannel(socket *websocket.Conn) {
+	for {
+		time.Sleep(time.Second * 1)
+		message := Message{"channel add",
+			Channel{"1","channel 1"}}
+		socket.WriteJSON(message)
+		fmt.Println("sent new channel")
+	}
+}
